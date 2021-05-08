5223966e85c947842dffc689bd4f451f450649b4
sadan kumar <sadanroy@gmail.com>
2021-05-08 14:17:35 +0530

    Add project files.

---
+++ CSharpConcepts/MainClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpConcepts
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int originalNumber = 10;
+            OutVsRef outVsRef = new OutVsRef();
+
+            outVsRef.AddNumbers(originalNumber);
+            Console.WriteLine("originalNumber number is - {0}", originalNumber);
+
+
+            outVsRef.AddNumbersUsingOut(out originalNumber);
+            Console.WriteLine("originalNumber number using out is - {0}", originalNumber);
+
+            outVsRef.AddNumbersUsingRef(ref originalNumber);
+            Console.WriteLine("originalNumber number using ref is - {0}", originalNumber);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CSharpConcepts/OutVsRef.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConcepts
+{
+    class OutVsRef
+    {
+        public void AddNumbers(int number)
+        {
+            number = number + 10;
+        }
+        public void AddNumbersUsingOut(out int number)
+        {
+            number = 0;// we cant pass value from caller function to callee , as we need to initialise it first
+            number = number + 10;
+        }
+        public void AddNumbersUsingRef(ref int number)
+        {
+            number = number + 10;
+        }
+    }
+}
