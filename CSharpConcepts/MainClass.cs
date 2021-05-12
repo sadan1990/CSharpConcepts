e44d48021b37ff00c47202e72584a1d1355c0939
sadan kumar <sadanroy@gmail.com>
2021-05-12 08:35:41 +0530

    singleton class changes

--- CSharpConcepts/MainClass.cs
+++ CSharpConcepts/MainClass.cs
@@ -8,6 +8,7 @@ namespace CSharpConcepts
         static void Main(string[] args)
         {
             ////////////////////////////Out vs Ref ////////////////////////////////////
+            Console.WriteLine("--------------------------------Out vs Ref--------------------------");
             int originalNumber = 10;
             OutVsRef outVsRef = new OutVsRef();
 
@@ -22,18 +23,30 @@ namespace CSharpConcepts
             Console.WriteLine("originalNumber number using ref is - {0}", originalNumber);
 
             /////////////////////////////Extension Method//////////////////////
+            Console.WriteLine("--------------------------------Extension Method--------------------------");
             string name = "sadan";
             string reverse = name.ReverseString();
             Console.WriteLine("reverse string using extension method is - {0}", reverse);
 
-            List<string> allNames = new List<string> {"sadan","sadanroy" ,"skumar","sprag"};
+            List<string> allNames = new List<string> { "sadan", "sadanroy", "skumar", "sprag" };
             //2 ways to get data
-            var result= allNames.NameContainsSadan(); //extension method .. behind the scenes it convert into next code line
+            var result = allNames.NameContainsSadan(); //extension method .. behind the scenes it convert into next code line
             var result1 = ExtensionMethod.NameContainsSadan(allNames);
 
             ////////////////////
-            ///
+            Console.WriteLine("--------------------------------Read only vs constant--------------------------");
             ConstantVsReadOnly.SomeMethod();
+
+
+
+            //////////////////////////////////Signleton Pattern////////////////////////////////
+
+            Console.WriteLine("--------------------------------Signleton Class--------------------------");
+            SigletonClass obj = SigletonClass.ReturnSigletonObject;
+            obj.PrintMessage();
+
+            SigletonClass obj1 = SigletonClass.ReturnSigletonObject;
+            obj1.PrintMessage();
 
             Console.ReadLine();
 
---
+++ CSharpConcepts/SigletonClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConcepts
+{
+    //Name of the class can be anything, here we are saying it sinleton
+    //Sealed class for internal class inheritance prevention(nested class)
+    sealed class SigletonClass
+    {
+        private static int counterForObjectCreation=0;
+        private static readonly SigletonClass obj = new SigletonClass();
+
+        //Private contructor prevents from object creation and external inheritance
+        private SigletonClass()
+        {
+            counterForObjectCreation++;
+            Console.WriteLine("object creation counter is - {0}", counterForObjectCreation);
+        }
+        public static SigletonClass ReturnSigletonObject
+        {
+            get
+            {
+                return obj;
+            }
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine("Priint Method Invoked");
+        }
+    }
+}
